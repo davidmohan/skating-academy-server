9654a4b1a0911ca661c5163fbcabe9d4bf52f1ab
Dowdy <admohan2002.gmail.com>
2023-06-07 12:31:40 +0530

    Project Server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
 
-app.use('/', router.router)
+app.use('/', router)
 
 app.listen(process.env.PORT, () => {
   console.log("Server Online")
